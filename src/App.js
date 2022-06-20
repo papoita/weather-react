d8a78994aba3ffc9765ca115f0950a92377a86b0
PaolaP <paolaperez8@gmail.com>
2022-06-20 15:27:32 -0400

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React from "react";
 import Form from "./Form";
-import Weather from "./Weather";
+import Footer from "./Footer";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>Weather App by Papo</h1>
-       <Weather/>
-       <Form />
-      </header>
+      <h1>Weather App by Papo</h1>
       
+      <Form />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Footer(){
+  return(<>
+  Coded by <a href="https://www.linkedin.com/in/perezleivapaola/" >Paola Perez</a> using <a href="https://github.com/papoita/weather-react" >open-Source code</a>
+  </>) ;
+}
